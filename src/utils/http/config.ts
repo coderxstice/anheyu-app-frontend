fe2768b227f71e2912138c85295215d6be34a978
陈志伟 <me@anheyu.com>
2025-06-12 11:20:17 +0800

    修复: 更新非生产环境的API地址，修正为正确的壁纸服务URL

--- src/utils/http/config.ts
+++ src/utils/http/config.ts
@@ -3,7 +3,7 @@
  * @Author: 安知鱼
  * @Email: anzhiyu-c@qq.com
  * @Date: 2023-02-26 20:12:37
- * @LastEditTime: 2025-04-12 23:54:44
+ * @LastEditTime: 2025-06-12 11:19:12
  * @LastEditors: 安知鱼
  */
 
@@ -11,8 +11,8 @@ let baseUrlApi;
 if (process.env.NODE_ENV === "production") {
   baseUrlApi = (url: string) => location.origin + `/api/${url}`;
 } else {
-  // baseUrlApi = (url: string) => `http://localhost:8091/api/${url}`;
-  baseUrlApi = (url: string) => `https://wallpaper.anheyu.com/api/${url}`;
+  baseUrlApi = (url: string) => `http://localhost:8091/api/${url}`;
+  // baseUrlApi = (url: string) => `https://album.anheyu.com/api/${url}`;
 }
 
 export { baseUrlApi };
