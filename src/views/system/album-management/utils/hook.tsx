b5bf7ee3587c1c57b0ca6a57cf1fcea2b5d96307
anzhiyu-c <me@anheyu.com>
2025-06-26 01:17:45 +0800

    feat: 优化加载动画配置，移除不必要的 spinner 定义

--- src/views/system/album-management/utils/hook.tsx
+++ src/views/system/album-management/utils/hook.tsx
@@ -382,17 +382,7 @@ export function useAlbum() {
   /** 加载动画配置 */
   const loadingConfig = reactive<LoadingConfig>({
     text: "正在加载第一页...",
-    viewBox: "-10, -10, 50, 50",
-    spinner: `
-        <path class="path" d="
-          M 30 15
-          L 28 17
-          M 25.61 25.61
-          A 15 15, 0, 0, 1, 15 30
-          A 15 15, 0, 1, 1, 27.99 7.5
-          L 15 15
-        " style="stroke-width: 4px; fill: rgba(0, 0, 0, 0)"/>
-      `
+    viewBox: "-10, -10, 50, 50"
     // svg: "",
     // background: rgba()
   });
