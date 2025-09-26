f60f37604f65d814f3489f8e0b0cf6a15ae7cfd8
anzhiyu-c <me@anheyu.com>
2025-09-26 15:14:15 +0800

    chore(config): 更新commitlint配置，调整规则以简化提交信息格式；更新版本号至1.1.19

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,11 +5,12 @@ export default {
   ignores: [commit => commit.includes("init")],
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "body-leading-blank": [2, "always"],
-    "footer-leading-blank": [1, "always"],
+    "body-leading-blank": [0, "never"],
+    "footer-leading-blank": [0, "never"],
     "header-max-length": [2, "always", 108],
     "subject-empty": [2, "never"],
     "type-empty": [2, "never"],
+    "scope-empty": [2, "never"],
     "type-enum": [
       2,
       "always",
