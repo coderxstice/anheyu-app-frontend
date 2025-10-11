da15ab030365a6fe46692ea038f7a9ec073da7a7
anzhiyu-c <me@anheyu.com>
2025-10-11 14:33:42 +0800

    refactor(post): 重构文章管理界面，优化搜索和操作区域布局，更新样式以提升用户体验

--- src/layout/components/lay-content/index.vue
+++ src/layout/components/lay-content/index.vue
@@ -124,7 +124,7 @@ const transitionMain = defineComponent({
                 transition: 'all 300ms cubic-bezier(0.4, 0, 0.2, 1)',
                 'border-radius': '0 0 30px 30px',
                 border: 'var(--style-border)',
-                background: 'var(--anzhiyu-card-bg-grey)'
+                background: 'var(--anzhiyu-background)'
               }"
               :view-style="{
                 display: 'flex',
